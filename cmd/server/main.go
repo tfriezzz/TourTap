a6c846b348e528e3f67ff3d0e6c6b3b23c3855e5
tfry <roswall.tim@gmail.com>
2026-01-20 15:34:07 +0900

    add server

---
+++ cmd/server/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+type apiConfig struct {
+	// db   *database.Queries
+	port string
+}
+
+func main() {
+	if err := godotenv.Load(); err != nil {
+		log.Fatalf("cannot load .env file")
+	}
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Fatal("PORT environment variable is not set")
+	}
+
+	cfg := apiConfig{
+		port: port,
+	}
+
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/health", http.HandlerFunc(cfg.handlerReadiness))
+
+	srv := &http.Server{
+		Addr:    ":" + port,
+		Handler: mux,
+	}
+
+	log.Printf("serving on: http://localhost:%s/\n", port)
+	log.Fatal(srv.ListenAndServe())
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/tfriezzz/tourtap
 
 go 1.25.3
+
+require github.com/joho/godotenv v1.5.1 // indirect
