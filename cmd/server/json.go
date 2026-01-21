56c158f02d59a4e91966cc6995fbda1a45c4e805
tfry <roswall.tim@gmail.com>
2026-01-22 07:57:24 +0900

    add respondWithError

--- cmd/server/json.go
+++ cmd/server/json.go
@@ -7,6 +7,18 @@ import (
 )
 
 func respondWithError(w http.ResponseWriter, code int, msg string, err error) {
+	if err != nil {
+		log.Println(err)
+	}
+	if code > 499 {
+		log.Printf("Responding with 5XX error: %s", msg)
+	}
+	type errorResponse struct {
+		Error string `json:"error"`
+	}
+	respondWithJSON(w, code, errorResponse{
+		Error: msg,
+	})
 }
 
 func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
