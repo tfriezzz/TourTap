424ecf16c01fb9c80536c84a620603b6f0fbfdd6
tfry <roswall.tim@gmail.com>
2026-01-29 11:39:55 +0900

    add renderer

--- internal/templates/templates.go
+++ internal/templates/templates.go
@@ -1,2 +1,21 @@
 // Package templates handles rendering/layout
 package templates
+
+import (
+	"net/http"
+	"text/template"
+)
+
+var tmpl *template.Template
+
+func Load() error {
+	tmpl = template.Must(template.ParseGlob("internal/templates/*.html"))
+	return nil
+}
+
+func Render(w http.ResponseWriter, name string, data any) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	if err := tmpl.ExecuteTemplate(w, name, data); err != nil {
+		http.Error(w, "Template error: "+err.Error(), http.StatusInternalServerError)
+	}
+}
