02cf1bfb5826cf039bf3ce25a8bc69da7220e309
tfry <roswall.tim@gmail.com>
2026-01-31 02:47:37 +0900

    add api.ts

---
+++ frontend/src/api.ts
@@ -0,0 +1,32 @@
+import axios from 'axios'
+import type { AxiosResponse } from 'axios'
+
+interface Tour {
+  id: number
+  name: string
+  created_at: string
+  updated_at: string
+  base_price: string
+}
+
+interface GroupRequestPayload {
+  requested_tour_id: number
+  requested_date: string
+  name: string
+  email: string
+  pax: number
+}
+
+const api = axios.create({
+  baseURL: '/api',
+})
+
+export const getTours = (): Promise<AxiosResponse<Tour[]>> => {
+  return api.get('/tours')
+}
+
+export const createGroupRequest = (
+  data: GroupRequestPayload
+): Promise<AxiosResponse> => {
+  return api.post('/groups/create', data)
+}
