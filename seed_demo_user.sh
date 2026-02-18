2abfaf2ce02b2260f1f39329298539700865c684
tfry <roswall.tim@gmail.com>
2026-02-18 11:20:15 +0900

    seed demo user

---
+++ seed_demo_user.sh
@@ -0,0 +1,23 @@
+
+#!/bin/sh
+
+echo "waiting for backend"
+
+until curl -s http://backend:8080/api/health > /dev/null; do
+  sleep 1
+done
+
+echo "seeding demo user..."
+
+curl -s -X POST http://backend:8080/api/users \
+  -H "Content-Type: application/json" \
+  -d '{
+    "password": "password",
+    "email": "test@email.com",
+    "name": "Test McTestson"
+  }'
+
+echo "done"
+
+
+
